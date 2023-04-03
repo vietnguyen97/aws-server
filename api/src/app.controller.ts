9f50e79017f967375a7d00181dde3ef1a4050469
vietnguyen97 <112857700+aiflocksvn@users.noreply.github.com>
2023-04-03 21:46:44 +0700

    update codebuild aws

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   createDynamoDb(): any {
     return this.appService.createDynamoDb();
   }
 
-  @Get('/write')
+  @Get("/write")
   writeDynamoDb() {
     return this.appService.writeDynamoDb();
   }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,36 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CreateTableCommand } from '@aws-sdk/client-dynamodb';
-import { PutCommand } from '@aws-sdk/lib-dynamodb';
-import { ddbClient } from '../config';
-import { params } from 'contants';
-import { ddbDocClient } from 'config/write-dynamodb';
+import { Injectable } from "@nestjs/common";
+import { CreateTableCommand } from "@aws-sdk/client-dynamodb";
+import { PutCommand } from "@aws-sdk/lib-dynamodb";
+import { ddbClient } from "../config";
+import { params } from "contants";
+import { ddbDocClient } from "config/write-dynamodb";
 
 @Injectable()
 export class AppService {
   async createDynamoDb(): Promise<any> {
     try {
       const data = await ddbClient.send(new CreateTableCommand(params));
-      console.log('Table Created', data);
+      console.log("Table Created new", data);
       return data;
     } catch (err) {
-      console.log('Error', err);
+      console.log("Error", err);
     }
   }
 
   async writeDynamoDb(): Promise<any> {
     const params = {
-      TableName: 'http-crud-tutorial-items',
+      TableName: "http-crud-tutorial-items",
       Item: {
-        id: '34',
-        name: 'Episode1',
-        price: '465',
+        id: "34",
+        name: "Episode1",
+        price: "465",
       },
     };
     try {
       const data = await ddbDocClient.send(new PutCommand(params));
-      console.log('Success - item added or updated', data);
+      console.log("Success - item added or updated", data);
     } catch (err) {
-      console.log('Error', err.stack);
+      console.log("Error", err.stack);
     }
   }
 }
